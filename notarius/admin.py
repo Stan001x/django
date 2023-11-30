376db68e3e63555968285ecfa1e0d1b6fd2207b1
Stanislav Shishkin <shishkinstas@mail.ru>
2023-11-30 17:19:01 +0700

    3011-1

--- notarius/admin.py
+++ notarius/admin.py
@@ -4,4 +4,18 @@ from .models import Report
 
 
 
-admin.site.register(Report)
+
+
+admin.site.site_header = "Приложение для формирование отчета"
+admin.site.site_title = "Title админки"
+admin.site.index_title = "Моя админка"
+
+class ReportAdmin(admin.ModelAdmin):
+    list_display = ('clientname', 'contractNumber', 'appraiser')
+    search_fields = ('clientname',)
+    list_editable = ("contractNumber",)
+    actions = ('make_zero',)
+    def make_zero(self, request, queryset):
+        queryset.update(contractNumber=0)
+
+admin.site.register(Report, ReportAdmin)
